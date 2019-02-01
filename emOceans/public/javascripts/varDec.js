4687f6ee66b9df420258512a06391098f154790a
Katie Duane <katiejduane@gmail.com>
2019-02-01 16:20:06 -0500

    friday pm organization

---
+++ emOceans/public/javascripts/varDec.js
@@ -0,0 +1,25 @@
+// BUTTON VARIABLE DECLARATIONS ==============================================>
+let lightenButton = document.getElementById('lighten');
+let darkenButton = document.getElementById('darken');
+let satButton = document.getElementById('sat');
+let desatButton = document.getElementById('desat');
+let warmButton = document.getElementById('warm');
+let coolButton = document.getElementById('cool');
+let resetButton = document.getElementById('reset');
+
+
+// Colored circles declarations =================================================>
+// let redCircle = document.getElementById('red');
+// let orangeCircle = document.getElementById('orange');
+// let yellowCircle = document.getElementById('yellow');
+// let greenCircle = document.getElementById('green');
+// let turquoiseCircle = document.getElementById('turquoise');
+// let blueCircle = document.getElementById('blue');
+// let purpleCircle = document.getElementById('purple');
+// let magentaCircle = document.getElementById('magenta');
+// let brownCircle = document.getElementById('brown');
+// let whiteCircle = document.getElementById('white');
+// let greyCircle = document.getElementById('grey');
+// let blackCircle = document.getElementById('black');
+
+// colors as variables ========================================================>
