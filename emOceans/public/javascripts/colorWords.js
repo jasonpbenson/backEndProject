194e81b0c96a338aeb37457303e81e0a917876a7
jasonpbenson <jasonbenson3rd@gmail.com>
2019-02-04 14:51:39 -0500

    mobile moodboard page and misc. other updates

--- emOceans/public/javascripts/colorWords.js
+++ emOceans/public/javascripts/colorWords.js
@@ -1,57 +1,81 @@
 const colors = [
     {
-        red: [`Love, romance, danger, attention, warmth, comfort, energy, excitement,
+        colorCode: "rgb(220, 20, 60)",
+        colorName: "Red",
+        attributes: [`Love, romance, danger, attention, warmth, comfort, energy, excitement,
         intensity, life, blood.`]
     },
 
     {
-        pink: [`Romance, love, gentleness, calm, tenderness.`]
+        colorCode: "rgb(255, 128, 0)",
+        colorName: "Pink",
+        attributes: [`Romance, love, gentleness, calm, tenderness.`]
     },
 
     {
-        orange: [`Happiness, energy, excitement, enthusiasm, warmth, prosperity,
+        colorCode: "rgb(255, 215, 0)",
+        colorName: "Orange",
+        attributes: [`Happiness, energy, excitement, enthusiasm, warmth, prosperity,
         change, stimulation, youthfulness.`]
     },
 
     {
-        yellow: [`Happiness, laughter, cheer, warmth, optimism, hunger, intensity,
+        colorCode: "rgb(255, 215, 0)",
+        colorName: "Yellow",
+        attributes: [`Happiness, laughter, cheer, warmth, optimism, hunger, intensity,
         sun, summer, levity.`]
     },
 
     {
-        green: [`Nature, coolness, growth, transformation, renewal, money, health, envy,
+        colorCode: "rgb(0, 128, 0)",
+        colorName: "Green",
+        attributes: [`Nature, coolness, growth, transformation, renewal, money, health, envy,
         tranquility, harmony, calm, fertility, good luck.`]
     },
 
     {
+        colorCode: "rgb(0, 139, 139)",
+        colorName: "Turquoise",
         turquoise: [`Femininity, sophistication, energy, wisdom, serenity, wholeness, creativity, 
         balance, spirituality, tranquility, patience, intuition, friendship, and loyalty.`]
     },
 
     {
+        colorCode: "rgb(0, 0, 255)",
+        colorName: "Blue",
         blue: [`Stability, calm, serenity, sadness, cold, distance, wisdom, loyalty,
         truth, focus.`]
     },
 
     {
+        colorCode: "rgb(102, 51, 153)",
+        colorName: "Purple",
         purple: [`Creativity, royalty, wealth, sophistication, wisdom, exotica, spirituality,
                 prosperity, respect, mystery.`]
     },
 
     {
+        colorCode: "rgb(255, 255, 255)",
+        colorName: "White",
         white: [`Purity, innocence, cleanliness, sense of space, neutrality, peace, surrender, quiet.`]
     },
 
     {
+        colorCode: "rgb(169, 169, 169)",
+        colorName: "Gray",
         gray: [`Neutrality, timelessness, practicality, boredom.`]
     },
     
     {
+        colorCode: "rgb(0, 0, 0)",
+        colorName: "Black",
         black: [`Authority, power, strength, evil, intelligence, death, mourning, night,
         darkness, space, depth.`]
     },
 
-    {
+    {   
+        colorCode: "rgb(139, 69, 19)",
+        colorName: "Brown",
         brown: [`Groundedness, reliability, earthy, friendship, warmth, comfort, security
         natural, organic.`]
     }
