a80718a11f79aaa7dbff35ec6c2f6e8f55844eef
Katie Duane <katiejduane@gmail.com>
2019-02-05 17:26:28 -0500

    updated JS bizniss

---
+++ emOceans/public/javascripts/navbar.js
@@ -0,0 +1,25 @@
+// function myNav(){
+//     let x = document.querySelector(".menu");
+//     if(x === active/hover){
+//         // navbar: "active";
+//     } else {
+//         // nabvar: "hidden";
+//     }
+// }
+let menu = document.querySelector('.hamburgerMenu');
+let hamburger = document.querySelector('.hamburger');
+// function toggleMenu() {
+//     let menuClassArray = [].slice.call(menu.classList)
+//     if(menuClassArray.includes('active')){  //used include instad of className because there are too many classes
+//         // menuClassArray.remove('active');
+//         menuClassArray = "hidden";
+
+//     }else{
+//         menuClassArray.add('active');   //THis is the function for it to work but we still have tell it when to do this.
+//     }
+//  }
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})   //THis is the function for it to work but we still have tell it when to do this.);
+
